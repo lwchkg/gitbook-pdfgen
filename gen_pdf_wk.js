c5652140ea53f86482a53806a3ea17c499ede8d7
WC Leung <lwchkg@gmail.com>
2017-12-03 14:58:52 +0000

    Add eslint and prettier
    Also fix code that trigger eslint errors.

--- gen_pdf_wk.js
+++ gen_pdf_wk.js
@@ -1,33 +1,37 @@
 #!/usr/bin/env node
-'use strict';
 
-const childProcess = require('child_process');
-const cmdOptions = require('commander');
-const dom = require('xmldom').DOMParser;
-const fs = require('fs-extra');
-const fsp = require('fs-promise');
-const fileUrl = require('file-url');
-const htmltidy = require('htmltidy2');
-const less = require('less');
-const path = require('path');
-const selectn = require('selectn');
-const streamToPromise = require('stream-to-promise');
-const xpath = require('xpath');
+const childProcess = require("child_process");
+const cmdOptions = require("commander");
+const dom = require("xmldom").DOMParser;
+const fs = require("fs-extra");
+const fsp = require("fs-promise");
+const fileUrl = require("file-url");
+const htmltidy = require("htmltidy2");
+const less = require("less");
+const path = require("path");
+const selectn = require("selectn");
+const streamToPromise = require("stream-to-promise");
+const xpath = require("xpath");
 
-const configLoader = require('./gen_pdf_wk_loadConfig.js');
+const configLoader = require("./gen_pdf_wk_loadConfig.js");
+
+// Module level variables.
+let config;
 
 // Convert string representing integers to a number. Throws if failed.
 function filterInt(value) {
-  if(/^(\-|\+)?([0-9]+|Infinity)$/.test(value))
+  if (/^(-|\+)?([0-9]+|Infinity)$/.test(value)) {
     return Number(value);
-  throw(new Error(`Unable to parse "${value}" as an integer.`));
+  }
+  throw new Error(`Unable to parse "${value}" as an integer.`);
 }
 
 // Wrapper around 'file-url' module. It converts Windows drive letters to lower
 // case so QT can understand them.
 function getQTCompatibleFileUrl(path) {
-  return fileUrl(path).replace(new RegExp('^file:///[A-Z]:/'),
-                               match => match.toLowerCase());
+  return fileUrl(path).replace(new RegExp("^file:///[A-Z]:/"), match =>
+    match.toLowerCase()
+  );
 }
 
 // Convenience function to get the source path of the asset.
@@ -46,10 +50,10 @@ class GitbookToWkhtmltopdf {
   }
 
   _getStdinArgs(args) {
-    let ret = '';
+    let ret = "";
     for (const arg of args) {
-      ret = ret + arg.toString().replace(/[ \t\n\r\\]/g, '\\$&');
-      ret = ret + ' ';
+      ret += arg.toString().replace(/[ \t\n\r\\]/g, "\\$&");
+      ret += " ";
     }
     return ret;
   }
@@ -59,84 +63,119 @@ class GitbookToWkhtmltopdf {
       let url;
       try {
         url = path.join(
-                  cmdOptions.ebookDirectory,
-                  decodeURIComponent(
-                      select('./html:span/html:a/@href', node)[0].value));
-      } catch(e) {
+          cmdOptions.ebookDirectory,
+          decodeURIComponent(select("./html:span/html:a/@href", node)[0].value)
+        );
+      } catch (e) {
         // Multipart titles - skip this item until we have a better handling method.
         continue;
       }
-      const title =
-          select('normalize-space(./html:span/html:a/text())', node).toString();
-      this._docList.push({'url' : url, 'title' : title, 'level' : level});
-      const subNodes = select('./html:ol/html:li', node);
+      const title = select(
+        "normalize-space(./html:span/html:a/text())",
+        node
+      ).toString();
+      this._docList.push({ url: url, title: title, level: level });
+      const subNodes = select("./html:ol/html:li", node);
       this._processNodes(select, subNodes, level + 1);
     }
   }
 
   _doCommand() {
-    let args = [];
-    args.push('-T', config.margin.top,
-              '-B', config.margin.bottom,
-              '-L', config.margin.left,
-              '-R', config.margin.right);
+    const args = [];
+    args.push(
+      "-T",
+      config.margin.top,
+      "-B",
+      config.margin.bottom,
+      "-L",
+      config.margin.left,
+      "-R",
+      config.margin.right
+    );
 
-    if (config.title) args.push('--title', config.title);
+    if (config.title) {
+      args.push("--title", config.title);
+    }
 
-    const argsForAllPages =
-        ['--zoom', cmdOptions.zoom, '--debug-javascript',
-         '--javascript-delay', cmdOptions.javascriptDelay];
+    const argsForAllPages = [
+      "--zoom",
+      cmdOptions.zoom,
+      "--debug-javascript",
+      "--javascript-delay",
+      cmdOptions.javascriptDelay
+    ];
 
     if (config.cover) {
-      args.push('cover',
-                getQTCompatibleFileUrl(this._getAssetDescPath('cover')),
-                '--exclude-from-outline', ...argsForAllPages);
+      args.push(
+        "cover",
+        getQTCompatibleFileUrl(this._getAssetDescPath("cover")),
+        "--exclude-from-outline",
+        ...argsForAllPages
+      );
     }
 
     // Can't use file:// URL in XSL stylesheet. The reason is unknown.
-    args.push('toc', ...argsForAllPages);
+    args.push("toc", ...argsForAllPages);
     if (config.tocXsl) {
-      args.push('--xsl-style-sheet',
-                path.relative(cmdOptions.ebookDirectory,
-                              this._getAssetDescPath('tocXsl')));
+      args.push(
+        "--xsl-style-sheet",
+        path.relative(
+          cmdOptions.ebookDirectory,
+          this._getAssetDescPath("tocXsl")
+        )
+      );
     }
 
-    let argsForNormalPages = [...argsForAllPages];
-    for (let section of ['header', 'footer']) {
-      let sectionConfig = config[section];
-      if (!sectionConfig.contentHtml) continue;
+    const argsForNormalPages = [...argsForAllPages];
+    for (const section of ["header", "footer"]) {
+      const sectionConfig = config[section];
+      if (!sectionConfig.contentHtml) {
+        continue;
+      }
       argsForNormalPages.push(
-          `--${section}-html`,
-          getQTCompatibleFileUrl(
-              this._getAssetDescPath(`${section}.contentHtml`)),
-          `--${section}-spacing`, sectionConfig.spacing);
+        `--${section}-html`,
+        getQTCompatibleFileUrl(
+          this._getAssetDescPath(`${section}.contentHtml`)
+        ),
+        `--${section}-spacing`,
+        sectionConfig.spacing
+      );
     }
     Object.freeze(argsForNormalPages);
 
-    for (const doc of this._docList)
+    for (const doc of this._docList) {
       args.push(getQTCompatibleFileUrl(doc.url), ...argsForNormalPages);
+    }
 
     args.push(path.relative(cmdOptions.ebookDirectory, cmdOptions.outputFile));
 
-    const cmd_options = {
+    const childProcessOptions = {
       cwd: cmdOptions.ebookDirectory,
       input: this._getStdinArgs(args),
-      stdio: ['pipe', process.stdout, process.stderr],
-      timeout: 200000  // wkhtmltopdf can infinitely loop in some cases.
+      stdio: ["pipe", process.stdout, process.stderr],
+      timeout: 200000 // wkhtmltopdf can infinitely loop in some cases.
     };
-    console.log('Launching wkhtmltopdf:');
-    if (cmdOptions.debug)
-      console.log('[debug] Options for wkhtmltopdf: %O', cmd_options);
-    childProcess.spawnSync('wkhtmltopdf', ['--read-args-from-stdin'], cmd_options);
+    console.log("Launching wkhtmltopdf:");
+    if (cmdOptions.debug) {
+      console.log("[debug] Options for wkhtmltopdf: %O", childProcessOptions);
+    }
+    childProcess.spawnSync(
+      "wkhtmltopdf",
+      ["--read-args-from-stdin"],
+      childProcessOptions
+    );
   }
 
   process(xml) {
     const doc = new dom().parseFromString(xml);
-    const select =
-        xpath.useNamespaces({'html': 'http://www.w3.org/1999/xhtml'});
+    const select = xpath.useNamespaces({
+      html: "http://www.w3.org/1999/xhtml"
+    });
 
-    const nodes =
-        select('//html:div[contains(@class,"toc")]/html:ol/html:li', doc);
+    const nodes = select(
+      '//html:div[contains(@class,"toc")]/html:ol/html:li',
+      doc
+    );
     this._processNodes(select, nodes, 1);
     this._doCommand();
   }
@@ -144,35 +183,45 @@ class GitbookToWkhtmltopdf {
 
 // Build with GitBook if needed.
 function maybeBuildBook() {
-  if (!cmdOptions.build && fs.existsSync(cmdOptions.ebookDirectory) &&
-      fs.statSync(cmdOptions.ebookDirectory).isDirectory()) {
+  if (
+    !cmdOptions.build &&
+    fs.existsSync(cmdOptions.ebookDirectory) &&
+    fs.statSync(cmdOptions.ebookDirectory).isDirectory()
+  ) {
     return;
   }
-  console.log('Running GitBook:');
-  let args = ['build', '.', cmdOptions.ebookDirectory, '--format', 'ebook'];
-  childProcess.spawnSync('gitbook', args, { shell: true, stdio: 'inherit' });
+  console.log("Running GitBook:");
+  const args = ["build", ".", cmdOptions.ebookDirectory, "--format", "ebook"];
+  childProcess.spawnSync("gitbook", args, { shell: true, stdio: "inherit" });
 }
 
 // Process the assets. Returns the mapping from source to destination.
 function processAssets() {
   const assets = new Set(
-     [...config.assets, config.cover,
-      selectn('header.contentHtml', config),
-      selectn('footer.contentHtml', config)]
-         .filter(v => v)
-         .map(v => path.join(config.root, v)));
+    [
+      ...config.assets,
+      config.cover,
+      selectn("header.contentHtml", config),
+      selectn("footer.contentHtml", config)
+    ]
+      .filter(v => v)
+      .map(v => path.join(config.root, v))
+  );
 
-  let assetMap = new Map();
-  let assetReverseMap = new Map();
+  const assetMap = new Map();
+  const assetReverseMap = new Map();
   for (const assetSrc of assets) {
-    const assetDestBase = path.join(cmdOptions.ebookDirectory,
-                                    path.basename(assetSrc));
+    const assetDestBase = path.join(
+      cmdOptions.ebookDirectory,
+      path.basename(assetSrc)
+    );
     let assetDest;
     if (assetReverseMap.has(assetDestBase)) {
       for (let i = 1; ; ++i) {
-        assetDest = assetDestBase + '_' + i;
-        if (!assetReverseMap.has(assetDest))
+        assetDest = assetDestBase + "_" + i;
+        if (!assetReverseMap.has(assetDest)) {
           break;
+        }
       }
     } else {
       assetDest = assetDestBase;
@@ -182,86 +231,122 @@ function processAssets() {
     assetReverseMap.set(assetDest, assetSrc);
   }
 
-  return new Promise((resolve, reject) => {
-    Promise.all([...assetMap].map(([src, dest]) => {
-      switch (path.extname(src).toLowerCase()) {
-      case '.less': {
-        console.log('Processing LESS asset: %s => %s', src, dest);
-        const cssDest = dest.replace(/\.less$/, '.css');
-        return fsp.readFile(src, 'utf8')
-                  .then(data => less.render(data, {filename: src}))
-                  .then(output => fsp.writeFile(cssDest, output.css, 'utf8'));
-      }
-      case '.xsl': {
-        console.log('Processing XSL asset: %s => %s', src, dest);
-        return fsp.readFile(src, 'utf8')
-                  .then(data => {
-                    // Replace parts of URL for QT XSL.
-                    let url = getQTCompatibleFileUrl(cmdOptions.ebookDirectory)
-                                  .replace('%20', ' ');
-                    let processedData = data.replace('%url_current_dir%', url);
-                    return fsp.writeFile(dest, processedData, 'utf8');
-                  });
-      }
-      default: {
-        console.log('Copying asset: %s => %s', src, dest);
-        return fsp.copy(src, dest, {clobber: true});
-      }}
-    }))
-    .then(() => resolve(assetMap));
+  return new Promise(resolve => {
+    Promise.all(
+      [...assetMap].map(([src, dest]) => {
+        switch (path.extname(src).toLowerCase()) {
+          case ".less": {
+            console.log("Processing LESS asset: %s => %s", src, dest);
+            const cssDest = dest.replace(/\.less$/, ".css");
+            return fsp
+              .readFile(src, "utf8")
+              .then(data => less.render(data, { filename: src }))
+              .then(output => fsp.writeFile(cssDest, output.css, "utf8"));
+          }
+          case ".xsl": {
+            console.log("Processing XSL asset: %s => %s", src, dest);
+            return fsp.readFile(src, "utf8").then(data => {
+              // Replace parts of URL for QT XSL.
+              const url = getQTCompatibleFileUrl(
+                cmdOptions.ebookDirectory
+              ).replace("%20", " ");
+              const processedData = data.replace("%url_current_dir%", url);
+              return fsp.writeFile(dest, processedData, "utf8");
+            });
+          }
+          default: {
+            console.log("Copying asset: %s => %s", src, dest);
+            return fsp.copy(src, dest, { clobber: true });
+          }
+        }
+      })
+    ).then(() => resolve(assetMap));
   });
 }
 
 // Process the summary. Return the processed summary as xhtml.
 function processSummary() {
   return streamToPromise(
-    fs.createReadStream(path.join(cmdOptions.ebookDirectory, 'SUMMARY.html'))
-      .pipe(htmltidy.createWorker({
-        'input-encoding': 'utf8',
-        'output-encoding': 'utf8',
-        'output-xhtml': true})))
-  .then(buffer => {
-    return fsp.writeFile(path.join(cmdOptions.ebookDirectory, 'SUMMARY.xhtml'),
-                         buffer, 'utf8')
-              .then(() => buffer.toString());
-  });
+    fs
+      .createReadStream(path.join(cmdOptions.ebookDirectory, "SUMMARY.html"))
+      .pipe(
+        htmltidy.createWorker({
+          "input-encoding": "utf8",
+          "output-encoding": "utf8",
+          "output-xhtml": true
+        })
+      )
+  ).then(buffer =>
+    fsp
+      .writeFile(
+        path.join(cmdOptions.ebookDirectory, "SUMMARY.xhtml"),
+        buffer,
+        "utf8"
+      )
+      .then(() => buffer.toString())
+  );
 }
 
+// Constants for command line options.
+const ebookDir = "_ebook";
+const outputFile = "book_wk.pdf";
+const qtSmartShrinkDefault = 1.2;
+const jsDelayDefault = 1000;
+
 cmdOptions
-    .version('0.0.1')
-    .option('-D, --ebook-directory <path>',
-            'Set the temp directory of the generated ebook. [default: _ebook] Caution: errors will happen if a delimeter (space, comma, semicolon, equal     ) is present in this option.',
-            '_ebook')
-    .option('-o, --output-file <file>',
-            'Set the output file. [default: book_wk.pdf]', 'book_wk.pdf')
-    .option('-z, --zoom <float>',
-            'Set the zoom level. The default is meant to offset QT smart shrink. [default: 1.2]',
-            parseFloat, 1.2)
-    .option('-j, --javascript-delay <msec>',
-            'Allow the specified number of milliseconds for the javascript to finish. [default: 1000]',
-            filterInt, 1000)
-    .option('-n, --no-build', 'Do not rebuild tempororary files.')
-    .option('--debug', 'Print debugging information.')
-    .parse(process.argv);
+  .version("0.0.1")
+  .option(
+    "-D, --ebook-directory <path>",
+    `Set the temp directory of the generated ebook. [default: ${ebookDir}] Caution: errors will happen if a delimeter (space, comma, semicolon, equal) is present in this option.`,
+    ebookDir
+  )
+  .option(
+    "-o, --output-file <file>",
+    `Set the output file. [default: ${outputFile}]`,
+    outputFile
+  )
+  .option(
+    "-z, --zoom <float>",
+    `Set the zoom level. The default is meant to offset QT smart shrink. [default: ${
+      qtSmartShrinkDefault
+    }]`,
+    parseFloat,
+    qtSmartShrinkDefault
+  )
+  .option(
+    "-j, --javascript-delay <msec>",
+    `Allow the specified number of milliseconds for the javascript to finish. [default: ${
+      jsDelayDefault
+    }]`,
+    filterInt,
+    jsDelayDefault
+  )
+  .option("-n, --no-build", "Do not rebuild tempororary files.")
+  .option("--debug", "Print debugging information.")
+  .parse(process.argv);
 
-let config;
 // Load config file.
-configLoader.loadConfig()
-.then(rawConfig => {
-  config = rawConfig.wkhtmltopdf;
+configLoader
+  .loadConfig()
+  .then(rawConfig => {
+    config = rawConfig.wkhtmltopdf;
 
-  // Some config in other sections are copied to the wkhtmltopdf config.
-  config.root = rawConfig.root;
-  if (!config.title) config.title = rawConfig.title;
-  config.summary = rawConfig.structure.summary;
+    // Some config in other sections are copied to the wkhtmltopdf config.
+    config.root = rawConfig.root;
+    if (!config.title) {
+      config.title = rawConfig.title;
+    }
+    config.summary = rawConfig.structure.summary;
 
-  Object.freeze(config);
+    Object.freeze(config);
 
-  maybeBuildBook();
+    maybeBuildBook();
 
-  return Promise.all([processSummary(), processAssets()]);
-})
-.then(([xml, assetMap]) => {
-  new GitbookToWkhtmltopdf(assetMap).process(xml);
-})
-.catch(err => {console.error(err.stack);});
+    return Promise.all([processSummary(), processAssets()]);
+  })
+  .then(([xml, assetMap]) => {
+    new GitbookToWkhtmltopdf(assetMap).process(xml);
+  })
+  .catch(err => {
+    console.error(err.stack);
+  });
--- gen_pdf_wk_loadConfig.js
+++ gen_pdf_wk_loadConfig.js
@@ -1,23 +1,22 @@
-const fsp = require('fs-promise');
-const jsonschema = require('jsonschema');
-const jsonSchemaDefaults = require('json-schema-defaults');
-const merge = require('merge');
-const path = require('path');
+const fsp = require("fs-promise");
+const jsonschema = require("jsonschema");
+const jsonSchemaDefaults = require("json-schema-defaults");
+const merge = require("merge");
+const path = require("path");
 
 /**
-    Load book.json or a specified file, and validate the content.
-    And return with mixing with the default values.
+  Load book.json or a specified file, and validate the content.
+  And return with mixing with the default values.
 
-    @param {filename} The file containing the Json object
-    @return {Object}
+  @param {string} filename The file containing the Json object
+  @return {Object}
 */
-function loadConfig(filename = 'book.json') {
+function loadConfig(filename = "book.json") {
   return Promise.all([
     fsp.readJson(filename),
-    fsp.readJson(path.join(__dirname, 'gen_pdf_wk_config_schema.json'))
-  ])
-  .then (([book, schema]) => {
-    new jsonschema.Validator().validate(book, schema, {throwError: true});
+    fsp.readJson(path.join(__dirname, "gen_pdf_wk_config_schema.json"))
+  ]).then(([book, schema]) => {
+    new jsonschema.Validator().validate(book, schema, { throwError: true });
     return merge.recursive(true, jsonSchemaDefaults(schema), book);
   });
 }
